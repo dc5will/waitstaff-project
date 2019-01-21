e1f9c6e3ff5f78775455d6b3fa6f5a5baef1fd83
William Wong <wwong1987@gmail.com>
2019-01-21 12:43:17 -0800

    added some function stubs to js file

--- main.js
+++ main.js
@@ -1,3 +1,81 @@
 'use strict';
 
 /* global $ */
+
+
+// store data in obj
+const STORE = {
+  price,
+  tax,
+  tip,
+  earnings,
+  mealCounter,
+  tipTotal,
+};
+
+// handle meal details
+function handleMealDetails() {
+
+}
+
+// take value of base meal price
+function baseMealPriceValue() {
+
+}
+
+// take value of tax rate
+function taxRatePercentageValue() {
+
+}
+
+// take value of tip percentage
+function tipPercentageValue() {
+
+}
+
+// calculate sub total formula
+function calculateSubtotal() {
+
+}
+
+// calculate tip amount formula
+function calculateTipAmount() {
+
+}
+
+// calculate total amount formula
+function calculateTotalAmount() {
+
+}
+
+// submit button should validate values in all input fields are numeric
+function submitValidation() {
+
+}
+
+// submit button handling after validation
+function handleSubmitValidation() {
+
+}
+
+// cancel button resets data
+function resetForm() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function handleCalculator() {
+
+}
